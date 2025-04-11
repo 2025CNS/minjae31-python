684bd0a97649edce7a934f485857229e4e352f91
minjae31 <kimminjae900317@gmail.com>
2025-04-10 21:51:48 -0700

    6069

---
+++ codeup/6069.py
@@ -0,0 +1,18 @@
+# 평가 내용
+# 평가 : 내용
+# A : best!!!
+# B : good!!
+# C : run!
+# D : slowly~
+# 나머지 문자들 : what?
+n = input()
+if n == 'A':
+    print("best!!!")
+elif n == 'B':
+    print("good!!")
+elif n == 'C':
+    print("run!")
+elif n == 'D':
+    print("slowly~")
+else:
+    print("what?")
