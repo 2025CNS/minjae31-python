34258d21856426484fed16e1bb4f93315a58b198
minjae31 <kimminjae900317@gmail.com>
2025-04-10 21:51:53 -0700

    6070

---
+++ codeup/6070.py
@@ -0,0 +1,14 @@
+# 월 : 계절 이름
+# 12, 1, 2 : winter
+#   3, 4, 5 : spring
+#   6, 7, 8 : summer
+#   9, 10, 11 : fall
+a = int(input())
+if a == 12 or a == 1 or a == 2:
+    print('winter')
+if a == 3 or a == 4 or a == 5:
+    print('spring')
+if a == 6 or a == 7 or a == 8:
+    print('summer')
+if a == 9 or a == 10 or a == 11:
+    print('fall')
